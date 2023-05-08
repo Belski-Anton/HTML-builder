b1745f7154b9868af947a92c5dd44a3571be2397
Anton Belski <anton.belski@gmail.com>
2023-05-08 16:42:16 +0300

    feat:add solution for task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
 
+const fs = require('fs');
+const path = require('path');
+
+
+const copyDir = () => {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) throw err;
+    fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
+      if (err) throw err;
+      files.forEach((file) => {
+        const original = path.join(path.join(__dirname, 'files'), file.name);
+        const copy = path.join(path.join(__dirname, 'files-copy'), file.name);
+        if (file.isDirectory()) {
+          copyDir(original,copy);
+        } else {
+          fs.copyFile(original,copy, (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+    });
+  });
+};
+
+copyDir('./files', './files-copy');
